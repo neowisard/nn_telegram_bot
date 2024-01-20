f8f396d37731c6804a25b125c26e48b7b7240a26
Nick Kutuzov <n@ktzv.ru>
2024-01-20 14:49:33 +0300

    extend timeouts

--- bot/openai_utils.py
+++ bot/openai_utils.py
@@ -12,11 +12,11 @@ if config.openai_api_base is not None:
 
 OPENAI_COMPLETION_OPTIONS = {
     "temperature": 0.7,
-    "max_tokens": 4000,
+    "max_tokens": 16000,
     "top_p": 1,
     "frequency_penalty": 0,
     "presence_penalty": 0,
-    "request_timeout": 60.0,
+    "request_timeout": 300.0,
 }
 
 
